03ed373f56947f8887311d0e2abe4f8f70789e7b
kiraliwhite <104891984+kiraliwhite@users.noreply.github.com>
2022-10-03 14:39:19 +0800

    Update block-number.js

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,12 +1,15 @@
-const { task } = require("hardhat/config")
+const { task } = require("hardhat/config");
 
 task("block-number", "Prints the current block number").setAction(
-  // const blockTask = async function() => {}
-  // async function blockTask() {}
+  //箭頭函式 加上名稱的寫法
+  //const blockTask = async function() => {}
+  //一般函式的寫法
+  //async function blockTask() {
+  //以下為箭頭函式 省略名稱的寫法 變為匿名函式
   async (taskArgs, hre) => {
-    const blockNumber = await hre.ethers.provider.getBlockNumber()
-    console.log(`Current block number: ${blockNumber}`)
+    const blockNumber = await hre.ethers.provider.getBlockNumber();
+    console.log(`Current block number: ${blockNumber}`);
   }
-)
+);
 
 module.exports = {}
